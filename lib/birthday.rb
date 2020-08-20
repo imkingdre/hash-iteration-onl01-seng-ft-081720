62fd44cefe43d8a566b2b22f025853b73dbe809c
Andre King <andreking1031@gmail.com>
2020-08-20 19:26:14 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-    if birthday_kids == 12
+    if birthday_kids === 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
       puts "You are too old for this."
